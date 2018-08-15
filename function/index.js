1c5baa2944d58cb7fd13862445b735efe66ae01f
David An <davidan@broadinstitute.org>
2018-08-15 13:45:45 -0400

    function template

---
+++ function/index.js
@@ -0,0 +1,10 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!Object} req HTTP request context.
+ * @param {!Object} res HTTP response context.
+ */
+exports.tos = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
