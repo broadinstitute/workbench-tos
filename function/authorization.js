fd7bcb038422e742c09dd5b86019dd81bba1ab1d
David An <davidan@genomebridge.org>
2018-09-21 16:04:43 -0400

    validate OAuth token audience/email/expires/etc [risk: medium] (#12)
    validate OAuth token audience/email/expires/etc

--- function/authorization.js
+++ function/authorization.js
@@ -1,21 +1,44 @@
 // Create persistent/pipelined http client for outbound requests
 const requestPromise = require('request-promise-native');
 const persistentRequest = requestPromise.defaults({forever: true});
+const _ = require('lodash');
 
-const { rejection } = require('./responseError');
+const { rejection, ResponseError } = require('./responseError');
+
+// fail-safe handling of potentially-missing require. In live deploys, this require
+// will exist because our deploy process will render it based on config.js.ctmpl.
+// if you have simply checked out the codebase and are running unit tests - without
+// rendering .ctmpls - the require will not exist. We want to allow this.
+//
+// Unit tests override the audiencePrefixes and emailSuffixes values.
+// In a live deploy, if the config is missing, we default to [], which equates to an
+// empty whitelist.
+let audiencePrefixes = [];
+let emailSuffixes = [];
+try {
+    const oauthConfig = require('./config');
+    audiencePrefixes = oauthConfig.audiencePrefixes;
+    emailSuffixes = oauthConfig.emailSuffixes;
+} catch (err) {
+    console.warn('could not load OAuth config require. You can ignore this warning if you are running unit tests and have not rendered ctmpls.');
+}
 
 // for replacing the "Bearer " prefix case-insensitively in header values
 const bearerPrefix = /^bearer /i;
 
-// define Authorizers as classes for ease of unit testing - unit tests can mock out parts of these classes.
-class Authorizer {
-    authorize() {
-        throw new Error('subclasses must implement');
-    }
-}
+// the object keys we expect to see in the OAuth token from Google
+const userInfoKeys = ['email', 'verified_email', 'user_id', 'audience', 'expires_in'];
 
-class GoogleOAuthAuthorizer extends Authorizer {
+// define GoogleOAuthAuthorizer as a class for ease of unit testing - unit tests can mock out parts of these classes.
+class GoogleOAuthAuthorizer {
     // TODO: move validation of Authorization request header into this class
+
+    // class constructor accepts config overrides, so unit tests can have some control
+    constructor(configObj) {
+        const config = configObj || {};
+        this.audiencePrefixes = config.audiencePrefixes || audiencePrefixes;
+        this.emailSuffixes = config.emailSuffixes || emailSuffixes;
+    }
 
     callTokenInfoApi(token) {
         const reqUrl = 'https://www.googleapis.com/oauth2/v2/tokeninfo?access_token=' + token;
@@ -28,6 +51,61 @@ class GoogleOAuthAuthorizer extends Authorizer {
             json: true
         };
         return persistentRequest.post(reqOptions)
+    }
+
+    validateAudienceOrEmail(audience, email) {
+        let valid = false;
+        // following two loops use for..in instead of Array.forEach to allow breaking out early
+
+        // does the audience value start with one of our whitelisted prefixes?
+        for (const i in this.audiencePrefixes) {
+            const prefix = this.audiencePrefixes[i];
+            if (audience.startsWith(prefix.toString())) {
+                valid = true;
+                break;
+            }
+        }
+        // if we haven't matched an audience,
+        // does the email value end with one of our whitelisted suffixes?
+        if (!valid) {
+            for (const i in this.emailSuffixes) {
+                const suffix = this.emailSuffixes[i];
+                if (email.endsWith(suffix.toString())) {
+                    valid = true;
+                    break;
+                }
+            }
+        }
+
+        return valid;
+    }
+
+    validateUserInfo(userinfo) {
+        // validate all fields exist
+        userInfoKeys.forEach( (key) => {
+            if (!userinfo.hasOwnProperty(key)) {
+                throw new ResponseError(`OAuth token does not include ${key}`, 401);
+            }
+        })
+        // validate verified_email is Boolean and true
+        const verified = userinfo.verified_email;
+        if (!_.isBoolean(verified)) {
+            throw new ResponseError('OAuth token verified_email must be a Boolean.', 401);
+        } else if (!verified) {
+            throw new ResponseError('OAuth token verified_email must be true.', 401);
+        }
+
+        // validate expires_in is numeric and > 0
+        const expires = _.toNumber(userinfo.expires_in);
+        if (isNaN(expires)) {
+            throw new ResponseError('OAuth token expires_in must be a number.', 401);
+        } else if (expires <= 0) {
+            throw new ResponseError(`OAuth token has expired (expires_in: ${expires})`, 401);
+        }
+        // validate audience/email
+        if (!this.validateAudienceOrEmail(userinfo.audience.toString(), userinfo.email.toString())) {
+            throw new ResponseError(`OAuth token must have an acceptable audience (${userinfo.audience}) or email (${userinfo.email})`, 401);
+        };
     }
 
     /**
@@ -44,7 +122,7 @@ class GoogleOAuthAuthorizer extends Authorizer {
             const token = authHeader.replace(bearerPrefix,'');
             return this.callTokenInfoApi(token)
                 .then((userinfo) => {
-                    // TODO: validate audience and/or whitelisted email suffixes
+                    this.validateUserInfo(userinfo);
                     return userinfo;
                 })
                 .catch((err) => {
@@ -57,10 +135,6 @@ class GoogleOAuthAuthorizer extends Authorizer {
             return rejection(401);
         }
     }
-}
-
-const getAuthorizer = function() {
-    return new GoogleOAuthAuthorizer();
 }
 
 module.exports = GoogleOAuthAuthorizer;
