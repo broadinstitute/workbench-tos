39f9a530bf3b2667a989e64f464ee852cce155a6
David An <davidan@broadinstitute.org>
2020-09-02 12:03:39 -0400

    AS-361: upgrade to nodejs10 runtime (#39)
    upgrade to nodejs10 runtime

--- deploy.sh
+++ deploy.sh
@@ -51,10 +51,12 @@ docker run --rm -v $PWD:${CODEBASE_PATH} \
 
 # Use google/cloud-sdk image to deploy the cloud function
 # TODO: is there a smaller version of this image we can use?
+# GCP_PROJECT env var was set automatically for nodejs 6 and 8, and the runtime code uses it.
+# but it is not set automatically for nodejs 10; therefore we set it here during deploy.
 docker run --rm -v $PWD:${CODEBASE_PATH} \
     -e BASE_URL="https://us-central1-broad-dsde-${ENVIRONMENT}.cloudfunctions.net" \
-    google/cloud-sdk:220.0.0 /bin/bash -c \
+    google/cloud-sdk:307.0.0 /bin/bash -c \
     "gcloud config set project ${PROJECT_NAME} &&
      gcloud auth activate-service-account --key-file ${CODEBASE_PATH}/${SERVICE_ACCT_KEY_FILE} &&
      cd ${CODEBASE_PATH} &&
-     gcloud functions deploy tos --source=./function --trigger-http --runtime nodejs8"
+     gcloud functions deploy tos --source=./function --trigger-http --runtime nodejs10 --set-env-vars GCP_PROJECT=${PROJECT_NAME}"
